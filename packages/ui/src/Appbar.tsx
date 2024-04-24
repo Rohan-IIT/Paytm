995e7fe12b2f8bc4d54856098608e16a24ccfc8e
Rohan-IIT <rpawar3@hawk.iit.edu>
2024-04-23 23:42:51 -0500

    CI added

--- packages/ui/src/Appbar.tsx
+++ packages/ui/src/Appbar.tsx
@@ -20,7 +20,6 @@ export const Appbar = ({
         </div>
         <div className="flex flex-col justify-center pt-2">
             <Button onClick={user ? onSignout : onSignin}>{user ? "Logout" : "Login"}</Button>
-
         </div>
     </div>
 }
